84602c21ab0e5bc9a6f84d46b0d8b9cf3c37cd12
Ilaria <arinii@usi.ch>
2021-02-22 00:16:01 +0100

    Piccole modifiche e aggiunta contatti

---
+++ script.js
@@ -0,0 +1,16 @@
+// var buttonClicker = function(plan, price){
+//     console.log(plan);
+//     console.log(price);
+// }
+//
+// var offer_buttons = document.getElementsByClassName("buttonPricing");
+// for(var i = 0; i < offer_buttons.length; i++){
+//     var curr_offer = offer_buttons[i];
+//     var plan_price = curr_offer.id.split("_");
+//     var plan = plan_price[0];
+//     var price = plan_price[1];
+//     curr_offer.onclick = function(plan, price){
+//         buttonClicker(plan, price);
+//     }
+// }
+//
