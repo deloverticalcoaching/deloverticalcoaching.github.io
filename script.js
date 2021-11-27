b53133e28f6284f515358869c9bc0a52522951e7
Ilaria <arinii@usi.ch>
2021-11-27 11:28:55 +0100

    New pages added

--- script.js
+++ script.js
@@ -15,3 +15,11 @@
 // }
 //
 
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
